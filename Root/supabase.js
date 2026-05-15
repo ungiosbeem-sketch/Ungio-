ed42b576bb74cb755c7691aa0c51f5caaf82c61a
ungiosbeem-sketch <ungiosbeem@gmail.com>
2026-05-15 16:33:57 +0300

    Update supabase.js

--- Root/supabase.js
+++ Root/supabase.js
@@ -1,6 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// Macluumaadkaaga rasmiga ah ee Supabase
 const supabaseUrl = 'https://ghtpmdxehfuswwxjemyt.supabase.co';
-const supabaseAnonKey = 'sb_publishable_YXOSNcMhtmNdkXwzZYS0oA_eKLcN6HL';
+const supabaseAnonKey = 'Sb_publishable_YXOSNcMhtmNdkXwzZYS0oA_eKLcN6HL';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
