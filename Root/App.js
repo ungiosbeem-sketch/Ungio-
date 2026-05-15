1a4676687f91f88b9e7d214ae9defff68309e921
ungiosbeem-sketch <ungiosbeem@gmail.com>
2026-05-15 16:28:25 +0300

    Update App.js

--- Root/App.js
+++ Root/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'react-native';
 
-// 1. Soo dejinta bogagga (Hubi in magacyadan ay u qoran yihiin sida GitHub-kaaga)
+// Soo dejinta boggaga (Hubi inay magacyadu isku mid yihiin GitHub-kaaga)
 import HomeScreen from './HomeScreen';
 import AddTaskScreen from './AddTaskScreen';
 import StatsScreen from './StatsScreen';
@@ -13,33 +14,30 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      {/* Waxaan ka dhigaynaa Status Bar-ka mid cad maadaama app-ku madow yahay */}
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
+      
       <Stack.Navigator 
         initialRouteName="Home"
-        screenOptions={{ 
-          headerShown: false,
-          animation: 'slide_from_right' // Animation qurux badan marka bogga la beddelo
+        screenOptions={{
+          headerShown: false, // Waxaan u daminnay si aan u isticmaalno custom headers
+          animation: 'slide_from_right', // Animation qurux badan marka boggaga la kala beddelo
+          contentStyle: { backgroundColor: '#000' } // Background-ka guud waa madow
         }}
       >
-        {/* Bogga koowaad ee Dashboard-ka */}
+        {/* Bogga Dashboard-ka */}
         <Stack.Screen name="Home" component={HomeScreen} />
         
-        {/* Bogga lagu daro hawlaha cusub */}
-        <Stack.Screen 
-          name="AddTask" 
-          component={AddTaskScreen} 
-          options={{ 
-            animation: 'slide_from_bottom',
-            presentation: 'modal' 
-          }} 
-        />
+        {/* Bogga lagu daro hawlaha (AddTask) */}
+        <Stack.Screen name="AddTask" component={AddTaskScreen} />
         
-        {/* Bogga Garaafyada iyo Horumarka */}
+        {/* Bogga Tirakoobka (Stats) */}
         <Stack.Screen name="Stats" component={StatsScreen} />
         
-        {/* Bogga macluumaadka isticmaalaha */}
+        {/* Bogga Profile-ka */}
         <Stack.Screen name="Profile" component={ProfileScreen} />
-
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+          }
